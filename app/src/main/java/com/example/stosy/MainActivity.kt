2aef7cc81f498760c30ed6ea022d54c85fc3a4ed
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-11-08 10:36:16 +0100

    gotowy stos

--- app/src/main/java/com/example/stosy/MainActivity.kt
+++ app/src/main/java/com/example/stosy/MainActivity.kt
@@ -9,39 +9,55 @@ import android.widget.TextView
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
 import java.util.LinkedList
+import java.util.Stack
 
 class MainActivity : AppCompatActivity() {
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         // Mateusz Blacha 3Pr
-        val text = findViewById<EditText>(R.id.txt).text.toString().toInt()
+        var text1 = findViewById<EditText>(R.id.txt)
         val add = findViewById<Button>(R.id.bt1_st)
         val del = findViewById<Button>(R.id.bt1_st2)
-        var text2 = findViewById<TextView>(R.id.txt2).text
+        var text2 = findViewById<TextView>(R.id.txt2)
         val first = findViewById<TextView>(R.id.bt1_st3)
         val last = findViewById<TextView>(R.id.bt1_st4)
-        val stos = LinkedList<Int>();
+        val stos = Stack<Int>()
+        text2.text = ""
 
 
-        if(stos.isEmpty()){
-            first.isInvisible
-            last.isInvisible
-            add.setOnClickListener {
-            stos.addFirst(text)
-            text2 = stos.toString()
-        }
-        }
-        else {
-            first.isVisible
-            last.isVisible
-            add.setOnClickListener {
-                stos.add(text)
-                text2 = stos.toString()
+        add.setOnClickListener {
+            if (text2.toString() != "") {
+                if (stos.isEmpty()) {
+                    stos.add(text1.text.toString().toInt())
+                    text2.text = stos.toString()
+
+                } else {
+                    first.isInvisible
+                    last.isInvisible
+                    stos.add(text1.text.toString().toInt())
+                    text2.text = stos.toString()
+                }
+
             }
         }
 
+        del.setOnClickListener {
+            stos.pop()
+            text2.text = stos.toString()
+        }
+
+        last.setOnClickListener {
+            if (!stos.isEmpty()) {
+                text2.text = "Ostatni element" + stos.last().toString()
+            }
+        }
+        first.setOnClickListener {
+            if (!stos.isEmpty()) {
+                text2.text = "Pierwszy element" + stos.first().toString()
+            }
+        }
     }
 }
